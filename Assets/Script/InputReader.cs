07a05cf7827eb6d0ace05d81e1c3d634ae0e9455
Leo <leo99brizuela@gmail.com>
2025-06-09 12:30:12 -0300

    Add input reader

---
+++ Assets/Script/InputReader.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System; // Necesario para usar Action
+
+public class InputReader : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset inputActions;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    public Vector2 MovementValue { get; private set; }
+
+    // Declaramos los eventos para el salto.
+    public event Action jumpStarted;
+    public event Action jumpCanceled;
+
+    private void Awake()
+    {
+        var map = inputActions.FindActionMap("Player");
+        moveAction = map.FindAction("Move");
+        jumpAction = map.FindAction("Jump");
+    }
+
+    private void OnEnable()
+    {
+        moveAction.started += ctx => MovementValue = ctx.ReadValue<Vector2>();
+        moveAction.performed += ctx => MovementValue = ctx.ReadValue<Vector2>();
+        moveAction.canceled += ctx => MovementValue = Vector2.zero;
+
+        // Disparamos los eventos cuando se inicia o cancela el salto
+        jumpAction.started += ctx => jumpStarted?.Invoke();
+        jumpAction.canceled += ctx => jumpCanceled?.Invoke();
+
+        moveAction.Enable();
+        jumpAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        jumpAction.Disable();
+    }
+}
