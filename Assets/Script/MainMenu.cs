9e6e9c74565d2ce426585e9d154ce3c2995c04d3
Leonardo <leo99brizuela@gmail.com>
2025-07-13 05:26:10 -0300

    Add main menu

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public string firstLevel;
+    public GameObject optionsScreen;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(firstLevel);
+    }
+
+    public void OpenOptions()
+    {
+        optionsScreen.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsScreen.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("quit");
+    }
+}
